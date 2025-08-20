c46c068c3c477bbceff7052a44a3ffe378de6b66
Sam Moore <smoore64@unc.edu>
2025-08-20 12:13:25 -0400

    Stub MMult functions

---
+++ R/MMult.R
@@ -0,0 +1,57 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+# 5 different methods of
+
+#0. Built in
+#1. Textbook Def as Sums
+#2. Distributed Mult from Left
+#3. Distributed Mult from Right
+#4. Outer Product of Inner Products
+#5. Inner Product of Outer Products
+
+
+mult.base <- function(A,B){
+
+
+}
+
+mult.textbook <- function(A,B) {
+
+
+
+}
+
+
+mult.LeftDist <- function (A,B) {
+
+
+}
+
+mult.DistRight <- function (A,B) {
+
+
+}
+
+
+mult.OuterInner <- function (A,B) {
+
+
+}
+
+mult.InnerOuter <- function (A,B) {
+
+
+}
