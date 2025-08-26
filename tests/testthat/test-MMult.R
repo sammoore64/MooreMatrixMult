b30e3b4d0c34096e832579407e1619400dc0b158
Sam Moore <smoore64@unc.edu>
2025-08-25 20:20:35 -0400

    Tested built in mult

--- tests/testthat/test-MMult.R
+++ tests/testthat/test-MMult.R
@@ -12,3 +12,11 @@ test_that("I know how to do matrix multiplication by hand", {
 })
 
 
+test_that("Built-in Multiplication Works",{
+  A <- matrix(1:4, nrow = 2, ncol = 2)
+  B <- matrix(1:6, nrow = 2, ncol = 3)
+  # computed by hand
+  prod <- matrix(c(7, 10, 15, 22, 23, 34), nrow = 2, ncol = 3)
+  expect_equal(mult.base(A, B), prod)
+}
+)
